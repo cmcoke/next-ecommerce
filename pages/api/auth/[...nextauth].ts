7dfc23743183f89061ad657338acfc2570c8b9a3
cmcoke <minoru.cm@gmail.com>
2023-05-04 12:14:46 -0500

    configured prisma with next auth and stripe

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,39 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import Stripe from "stripe";
+
+const prisma = new PrismaClient();
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+    })
+    // Add another provider (Apple, Facebook, Instgram, Github, etc...)
+  ],
+  // creates a new stripe customer when a new user logs in
+  events: {
+    createUser: async ({ user }) => {
+      // initiate stripe
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+        apiVersion: "2022-11-15"
+      });
+      // create a stripe customer
+      if (user.name && user.email) {
+        const customer = await stripe.customers.create({
+          email: user.email,
+          name: user.email
+        });
+        // update prisma user with the stripeCustomerId
+        await prisma.user.update({
+          where: { id: user.id },
+          data: { stripeCustomerId: customer.id }
+        });
+      }
+    }
+  }
+});
